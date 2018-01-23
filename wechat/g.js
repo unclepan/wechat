4ec743bddd1bf56e1095e69e4ba24f86052bcbf2
yangpan <738912168@qq.com>
2018-01-23 17:38:35 +0800

    feat(): 验证消息的确来自微信服务器以及获取access_token

---
+++ wechat/g.js
@@ -0,0 +1,91 @@
+'use strict'
+
+const sha1 = require('sha1');
+const request = require('request'); // request 是一个发送http请求的库
+
+const prefix = 'https://api.weixin.qq.com/cgi-bin/';
+const api = {
+    accessToken: `${prefix}token?grant_type=client_credential`,
+};
+
+function Wechat(opts) {
+    this.appID = opts.appID;
+    this.appSecret = opts.appSecret;
+    this.getAccessToken = opts.getAccessToken;
+    this.saveAccessToken = opts.saveAccessToken;
+
+    this.getAccessToken()
+    .then((data) => {
+        try {
+            data = JSON.parse(data);
+        } catch(e) {
+            return this.updateAccessToken();
+        }
+        if(this.isValidAccessToken(data)) {
+            return Promise.resolve(data);
+        } else {
+            return this.updateAccessToken();
+        }
+    })
+    .then((data) => {
+        this.access_token = data.access_token;
+        this.expires_in = data.expires_in;
+        this.saveAccessToken(data);
+    })
+    .catch((error) => {
+        console.log(error.message);
+    });
+};
+
+Wechat.prototype.isValidAccessToken = function(data) { // 验证票据的方法
+    if (!data || !data.access_token || !data.expires_in) {
+        return false;
+    }
+    const access_token = data.access_token; // 票据
+    const expires_in = data.expires_in; // 过期时间
+    const now = (new Date().getTime());
+    if(now < expires_in) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+Wechat.prototype.updateAccessToken = function(data) { // 更新票据的方法
+    const appID = this.appID;
+    const appSecret = this.appSecret;
+    const url = `${api.accessToken}&appid=${appID}&secret=${appSecret}`;
+
+    return new Promise(function(resolve, reject) {
+        request({url: url, json: true}, (error, response, body) => {
+            if (!error && response.statusCode == 200) {
+                const now = new Date().getTime();
+                const expires_in = now + (body.expires_in  - 20) * 1000;
+                body.expires_in = expires_in;
+                resolve(body);
+            }
+        });
+    })
+    .catch((error) => {
+        console.log(error.message);
+    });
+}
+
+module.exports = function(opts) {
+    const wechat = new Wechat(opts);
+    return async (ctx, next) =>  {
+        const token = opts.token;
+        const signature = ctx.query.signature;
+        const nonce = ctx.query.nonce;
+        const timestamp = ctx.query.timestamp;
+        const echostr = ctx.query.echostr;
+        const str = [token, timestamp, nonce].sort().join('');
+        const sha = sha1(str);
+        if(sha === signature) {
+            ctx.body = echostr  + '';
+        } else {
+            ctx.body = 'wrog';
+        }
+    };
+    
+}
